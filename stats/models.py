c12e073e1a7efdc47959963d3916bf8c2fc40f78
Jason Williams <jase.williams@gmail.com>
2015-11-11 00:21:11 +0000

    importing Sum

--- stats/models.py
+++ stats/models.py
@@ -15,6 +15,7 @@ import re
 import hashlib
 
 from django.db import models
+from django.db.models import Sum
 from django.utils import timezone
 from django.db import connections
 from django.utils.encoding import python_2_unicode_compatible
