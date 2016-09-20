7ba76bb13f3e9cc4b8844351596c0d7871871b4b
Jayflux <jase.williams@gmail.com>
2016-09-21 00:50:02 +0100

    changing to simple urls

--- hashweb/urls.py
+++ hashweb/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^api/stats/', include('stats.api_urls')),
     url(r'^stats/', include('stats.urls')),
     url(r'^eurostats$', views.euro_stats),
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     # url(r'^stats', 'stats.views.index'),
     url(r'^$', views.index)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
